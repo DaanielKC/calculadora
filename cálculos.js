431630aa4bba402bc5ae01319ca2cc271bf86ca4
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-03-29 22:57:09 -0300

    mais coisa e ajustes

---
+++ cálculos.js
@@ -0,0 +1,63 @@
+let num = document.querySelector('div#num')
+
+function apagar() {
+    num.innerHTML = ''
+}
+
+function um() {
+    num.innerHTML += '1'
+}
+
+function dois() {
+    num.innerHTML += '2'
+}
+
+function tres() {
+    num.innerHTML += '3'
+}
+
+function quatro(){
+    num.innerHTML += '4'
+}
+
+function cinco() {
+    num.innerHTML += '5'
+}
+
+function seis() {
+    num.innerHTML += '6'
+}
+
+function sete() {
+    num.innerHTML += '7'
+}
+
+function oito() {
+    num.innerHTML += '8'
+}
+
+function nove() {
+    num.innerHTML += '9'
+}
+
+function zero() {
+    num.innerHTML += '0'
+}
+
+function somar() {
+    num.innerHTML += ' + '
+}
+
+function subtrair() {
+    num.innerHTML += ' - '
+}
+
+
+
+
+
+
+
+function igual() {
+    
+}
